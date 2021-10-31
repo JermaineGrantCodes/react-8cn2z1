d5b1d06300e5d36d6e38dac5cfab76c7295f77cd
JermaineGrantCodes <jermaine_grant@live.co.uk>
2021-10-31 18:15:44 +0000

    2 files modified

--- src/MedicalTreatment.js
+++ src/MedicalTreatment.js
@@ -1,29 +1,13 @@
 
 import React from "react";
 
-function MedicalTreatment (){
+function MedicalTreatment (props){
 //React Hooks:
 
-const [productName, setProductName] = React.useState();
-const clearInterval = (event) =>{
-  setProductName("");
-}
-
-function onChangeHandler(event){
-let someValue = event.target.value;
-setProductName(someValue);
-
-}
 
 return (
   <div>
-<input placeholder="text field" value={productName} onChange = {onChangeHandler} />
-<button onClick = {event => clearInterval()}>
-  Clear
-  </button>
-
-<br/>
-{productName}
+This is a {props.Id}
 
 
   </div>
